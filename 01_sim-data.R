77f7a19a9889cdb4e4b8443ef0d4168e5b2a4b8d
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-09-28 10:24:46 -0400

    add sim  data

---
+++ 01_sim-data.R
@@ -0,0 +1,33 @@
+library(purrr)
+library(rstan)
+library(brms)
+
+# dimensions
+M <- 3L
+K <- 5L
+J <- 10L
+N <- 400L
+
+# hyperparameter
+alpha <- c(2, 1.5, 1)
+
+# priors
+psi <- rdirichlet(1, alpha)
+Z_table <- rmultinom(N, 1, psi)
+Z <-  map_dbl(1:N,  ~which(Z_table[, .x] == 1) - 1)
+
+# set parameters
+theta_z <- list(
+  `0` = c(0.97, 0.01, 0.02),
+  `1` = c(0.01, 0.97, 0.02),
+  `2` = c(0.49, 0.49, 0.02)
+)
+
+# Generate data
+Y <- array(NA, dim = c(N, J))
+for (j in 1:J) {
+  theta_i <- map(as.character(Z), ~ theta_z[[.x]])
+  y_j <- map_dbl(theta_i, ~which(rmultinom(1, 1, .x) == 1) - 1)
+  Y[, j] <- y_j
+}
+
