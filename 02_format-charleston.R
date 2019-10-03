9c352f8100660cfcd40e19b43aabc9434cac65c7
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-02 22:46:12 -0400

    data

---
+++ 02_format-charleston.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+ch_raw <- read_rds("data/charleston-2018.Rds")
+
+
+ch_votes <- ch_raw %>%
+  select(1:5, matches("party")) %>%
+  select(-PTY_party)
+
+
+ch_split <- ch_votes %>%
+  rename_at(vars(matches("(USH|HOU|CCD|JPR)_party")), ~str_replace(.x, "_party", "_split")) %>%
+  mutate_at(
+    vars(matches("_split")),
+    ~case_when(GOV_party == .x & GOV_party %in% c(-1, 1) ~ 0,
+               GOV_party != .x & GOV_party %in% c(-1, 1)  & .x %in% c(-1, 1) ~ 1,
+               is.na(.x) ~ NA_real_,
+               TRUE ~ 0)
+    ) %>%
+  mutate(GOV_party = recode(GOV_party, `1` = "R", `-1` = "D", `0` = "A"))
+
+
+ch_split %>% sample_n(10) %>%
+  select(5:10)
+
+
+write_rds(ch_split, "data/split_ch-2018.Rds")
