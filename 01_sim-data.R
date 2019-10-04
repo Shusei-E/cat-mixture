13c9ba5d67622a8b66758862c3f65003b605c7f3
Shusei Eshima <shusei.eshima@gmail.com>
2019-10-04 12:54:26 -0400

    working

--- 01_sim-data.R
+++ 01_sim-data.R
@@ -7,26 +7,26 @@ set.seed(02138)
 
 # dimensions
 M <- 2L
-K <- 5L
-D <- 8L
+K <- 3L
+D <- 5L
 N <- 400L
 
 # hyperparameter
-alpha <- c(3, 1, 1, 1, 1)
+alpha <- c(1, 1, 1)
 
 
 # cluster assignment
-pi <- rdirichlet(1, alpha)
-Z_table <- rmultinom(N, 1, pi)
+theta <- rdirichlet(1, alpha)
+Z_table <- rmultinom(N, 1, theta)
 Z <-  map_dbl(1:N,  ~which(Z_table[, .x] == 1))
 
 # set parameters
 mu <- list(
-  `1` = rep(.01, D),
-  `2` = rep(.05, D),
-  `3` = rep(.10, D),
-  `4` = rep(.99, D),
-  `5` = rbeta(D, 2, 5)
+  `1` = rnorm(D, 0, 3),
+  `2` = rnorm(D, 0, 3),
+  `3` = rnorm(D, 0, 3)
+  # `4` = rnorm(D, 0, 3),
+  # `5` = rnorm(D, 0, 3)
 )
 
 # each mu vector is K by D
@@ -36,7 +36,8 @@ stopifnot(all(map_lgl(mu, ~length(.x) == D)))
 # Generate data
 y <- array(NA, dim = c(N, D))
 for (i in 1:N) {
-  y[i, ] <- rbinom(D, size = 1, prob = mu[[Z[i]]])
+  # y[i, ] <- rbinom(D, size = 1, prob = mu[[Z[i]]])
+  y[i, ] <- rnorm(D, mean = mu[[Z[i]]], sd=0.05)
 }
 
 # put together data
@@ -47,7 +48,7 @@ data <- list(D = D,
              alpha = alpha)
 
 # target params
-params <- list(pi = pi,
+params <- list(theta = theta,
                mu = mu,
                Z = Z)
 
@@ -58,3 +59,9 @@ write_rds(params, "data/sim-params.Rds")
 k_vanilla <- kmeans(data$y, centers = K)
 kdf_vanilla <- as_tibble(k_vanilla$centers) %>%
   mutate(n = k_vanilla$size)
+
+# Gaussian Mixture
+library(mclust)
+gmm <- Mclust(y, G=K)
+summary(gmm)
+table(Z)
--- 03_fit-stan_on-sim.R
+++ 03_fit-stan_on-sim.R
@@ -9,26 +9,30 @@ params <- read_rds("data/sim-params.Rds")
 stanmodel <- stan_model("finite-mixture_stan-05-03.stan")
 stanout <- rstan::sampling(stanmodel,
                            data,
-                           chains = 4,
-                           iter = 2e3,
-                           thin = 1)
+                           chains = 1,
+                           iter = 1000,
+                           thin = 1,
+													 )
+
+stanmodel2 <- stan_model("finite-mixture_stan_bern.stan")
+stanout <- rstan::vb(stanmodel2, data)
 
 # save mixture model
 write_rds(stanout, "data/mcmc/stan-fit.Rds")
 
 
 # plot output --
-pi_mixture <- params$pi
+theta <- params$theta
 mu_true <- params$mu
-pi_label <- glue("[{str_c(round(pi_mixture[1, ], 2), collapse = ', ')}]")
+pi_label <- glue("[{str_c(round(theta[1, ], 2), collapse = ', ')}]")
 mu_label <- glue("[{str_c(round(mu_true[[1]], 2), collapse = ', ')}]")
 
 
-stan_plot(stanout, "pi_mixture") +
+stan_plot(stanout, "theta") +
   labs(caption = glue("True simplex is {pi_label}"))
 ggsave("figures/sim_posterior_pi.png", w = 5, h = 3)
 
-stan_trace(stanout, "pi_mixture", alpha = 0.3, nrow = 1)  +
+stan_trace(stanout, "theta", alpha = 0.3, nrow = 1)  +
   labs(caption = glue("True simplex is {pi_label}"))
 ggsave("figures/sim_trace_pi.png", w = 10, h = 3)
 
