6df28dc77365f95452ae4863181bfd3eaa4e6eba
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-06 18:34:47 -0400

    costly, but compute log likelihood at each t

--- 05_diagnose-EM.R
+++ 05_diagnose-EM.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(foreach)
+library(ggthemes)
+library(lemon)
 library(glue)
 
 # Data --------
@@ -39,15 +41,14 @@ loglik_obs <- function(t, obj = store_iter) {
   sum(loglik_obs)
 }
 
-loglik_obs(1)
-loglik_obs(10)
+
 
 #'extract parameters from iteration t
 #' @param t iteration index
 #' @param obj the EM object with all iterations
 #'
 #' @return A tibble
-vector_params <- function(t, obj = store_iter) {
+vector_params <- function(t, loglik = FALSE, obj = store_iter) {
   theta_vector <- obj[[t]]$theta
   mu_vector <- obj[[t]]$mu[, , (data$L + 1)]
   df_i <- tibble(param_id = 1:(length(theta_vector) + length(mu_vector)),
@@ -55,13 +56,14 @@ vector_params <- function(t, obj = store_iter) {
                           rep("mu",    length(mu_vector))),
                  values = c(theta_vector, mu_vector)
   )
+  if (loglik) df_i$loglik_obs <- loglik_obs(t)
   return(df_i)
 }
 
 # stack all pre-post comparisons
 params_stacked <- foreach(t = 2:length(store_iter), .combine = "bind_rows") %do% {
-  params_t <- vector_params(t)
-  params_tminus1 <- vector_params(t - 1)
+  params_t <- vector_params(t, loglik = TRUE)
+  params_tminus1 <- vector_params(t - 1, loglik = FALSE)
 
   left_join(params_tminus1, params_t,
             by = c("param_id", "type"),
@@ -74,11 +76,21 @@ params_stacked <- foreach(t = 2:length(store_iter), .combine = "bind_rows") %do%
 # plot max of diff
 params_stacked %>%
   group_by(iter) %>%
-  summarize(max_diff = max(diff)) %>%
-  ggplot(aes(iter, max_diff)) +
-  geom_point() +
+  summarize(`Maximum Change in Parameter (probability scale)` = max(diff),
+            `Observed Log Likelihood` = unique(loglik_obs)) %>%
+  pivot_longer(cols = -c(iter), names_to = "metric", values_to = "value") %>%
+  ggplot(aes(iter, value)) +
+  facet_rep_wrap(~metric, scales = "free_y", ncol = 1) +
+  coord_capped_cart(bottom='both', left = 'both') +
+  geom_point(size = 0.5) +
   geom_line() +
-  labs(x = "Iteration",
-       y = "max(change in parameter value)",
-       caption = glue("the parmater vector is {data$K} theta's and {data$K*data$D} mu's combined."))
-ggsave("figures/sim_EM_change-in-params.png", w = 4, h = 3)
+  theme_clean() +
+  theme(plot.background = element_rect(color = NA),
+        axis.line = element_line(color = "black"),
+        plot.title = element_text(hjust = 0.5, face = "bold"),
+        plot.caption = element_text(size = 6)) +
+  labs(x = "EM Iteration",
+       y = "Metric",
+       caption = glue("Note: The parmater vector is the estimated theta's and mu's combined.
+                      Higher observed log likelihood and llower sup-norms both indicate better fit."))
+ggsave("figures/sim_EM_change-in-params.png", w = 4, h = 4)
