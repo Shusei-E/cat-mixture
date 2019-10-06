75a782b989a97ab271755061a457b7c66c7f0d40
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-06 16:59:28 -0400

    smaller K

--- 01_sim-data.R
+++ 01_sim-data.R
@@ -8,11 +8,11 @@ set.seed(02138)
 # dimensions
 L <- 1L
 D <- 8L
-N <- 500L
-K <- 5L
+N <- 300L
+K <- 3L
 
 # hyperparameter
-alpha <- (K:1)^2
+alpha <- (K:1)
 
 
 # cluster assignment
@@ -20,13 +20,11 @@ theta <- rdirichlet(1, alpha)
 Z_table <- rmultinom(N, 1, theta)
 Z <-  map_dbl(1:N,  ~which(Z_table[, .x] == 1))
 
-# setpi parameters
+# set theta parameters
 mu <- list(
-  `1` = rep(0.01, D),
-  `2` = rep(0.05, D),
-  `3` = rep(0.10, D),
-  `4` = rep(0.99, D),
-  `5` = rbeta(D, 2, 5)
+  `1` = rep(0.05, D),
+  `2` = rep(0.95, D),
+  `3` = rbeta(D, 2, 5)
 )
 
 # each mu vector is K by D
@@ -57,15 +55,15 @@ write_rds(params, "data/sim-params.Rds")
 
 # check vanilla k means
 k_vanilla <- kmeans(data$y, centers = K)
-kdf_vanilla <- as_tibble(k_vanilla$centers) %>%
+as_tibble(k_vanilla$centers) %>%
   mutate(n = k_vanilla$size)
 
 # check sample mean | correct cluster assignment
 as_tibble(data$y) %>%
   mutate(cluster = Z) %>%
   group_by(cluster) %>%
-  summarize_if(is.double, mean)
+  summarize_all(mean)
 
 as_tibble(data$y) %>%
   mutate(profile = glue("{V1}{V2}{V3}{V4}{V5}{V6}{V7}{V8}")) %>%
-  count(profile)
+  count(profile, sort = TRUE)
