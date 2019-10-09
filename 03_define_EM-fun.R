3bfebc014cad70e1e61899acd5845f2ab8184be5
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-08 23:06:35 -0400

    move all functions, incl. diagnosis, to 3

--- 03_define_EM-fun.R
+++ 03_define_EM-fun.R
@@ -2,6 +2,58 @@ library(tidyverse)
 library(foreach)
 library(glue)
 
+
+# Diagnosis ----
+
+# check sup-norm of changes in model parameters
+
+#' Observed log likelihood
+#'
+#' @param t iteration index
+#' @param obj the EM object with all iterations
+#' @return the log likelihood
+#'
+loglik_obs <- function(t, obj = store_iter) {
+  loglik_obs <- c()
+
+  theta_t <- obj[[t]]$theta
+  mu_t <- obj[[t]]$mu
+  user_K <- length(theta_t)
+
+  # for each N
+  for (u in 1:data$U) {
+    resp_i <- c()
+    for (k in 1:user_K) {
+      resp_i[k] = foreach(j = 1:data$D, .combine = "*") %:%
+        foreach(l = 0:(data$L), .combine = "*") %do% {
+          log(mu_t[k, j, (l + 1)])^(data$uy[u, j] == l)
+        }
+    }
+    loglik_obs[u] <- data$n_u[u] * log(theta_t %*% resp_i)
+  }
+  sum(loglik_obs)
+}
+
+
+
+#'extract parameters from iteration t
+#' @param t iteration index
+#' @param obj the EM object with all iterations
+#'
+#' @return A tibble
+vector_params <- function(t, loglik = FALSE, obj = store_iter) {
+  theta_vector <- obj[[t]]$theta
+  mu_vector <- obj[[t]]$mu[, , (data$L + 1)]
+  df_i <- tibble(param_id = 1:(length(theta_vector) + length(mu_vector)),
+                 type = c(rep("theta", length(theta_vector)),
+                          rep("mu",    length(mu_vector))),
+                 values = c(theta_vector, mu_vector)
+  )
+  if (loglik) df_i$loglik_obs <- loglik_obs(t)
+  return(df_i)
+}
+
+
 #' Compute EM
 cat_mixture <- function(data, user_K = 3, n_iter = 100, fast = TRUE) {
 
@@ -13,15 +65,13 @@ cat_mixture <- function(data, user_K = 3, n_iter = 100, fast = TRUE) {
   }
 
   # Setup -------
-  user_K <- 3
-
   # initialize theta {K x 1}
   init_theta = rep(1/user_K, user_K)
   # init_theta = params$theta # chat by giving it  correct thetas?
 
   # initialize mu {K x D x L}x
-  init_Z_table <- rmultinom(data$N, size = 1, prob = init_theta)
-  init_Z <- map_dbl(1:data$N, ~which(init_Z_table[, .x] == 1))
+  init_Z_table = rmultinom(data$N, size = 1, prob = init_theta)
+  init_Z = map_dbl(1:data$N, ~which(init_Z_table[, .x] == 1))
 
   # data$L
   mu <- init_mu <- array(NA, dim = c(user_K, data$D, data$L + 1))
@@ -102,4 +152,21 @@ cat_mixture <- function(data, user_K = 3, n_iter = 100, fast = TRUE) {
     iter = iter + 1
   }
   return(store_iter)
+}
+
+
+# Summarize ----
+
+# stack all pre-post comparisons
+summ_params <- function(store_iter, calc_loglik = TRUE) {
+  foreach(t = 2:length(store_iter), .combine = "bind_rows") %do% {
+    params_t <- vector_params(t, loglik = calc_loglik)
+    params_tminus1 <- vector_params(t - 1, loglik = FALSE)
+
+    left_join(params_tminus1, params_t,
+              by = c("param_id", "type"),
+              suffix = c("_pre", "_now")) %>%
+      mutate(iter = t) %>%
+      mutate(diff = abs(values_now - values_pre))
+  }
 }
--- 05_diagnose-EM.R
+++ 05_diagnose-EM.R
@@ -4,73 +4,17 @@ library(ggthemes)
 library(lemon)
 library(glue)
 
+source("03_define_EM-fun.R")
+
 # Data --------
 data <- read_rds("data/sim-data.Rds")
 params <- read_rds("data/sim-params.Rds")
-
 store_iter <- read_rds("data/EM/sim-iterations.Rds")
+ch_iter <- read_rds("data/EM/charleston-iterations_trichotomous.Rds")
 
 # Diagnostics ---------
-
-# check sup-norm of changes in model parameters
-
-#' Observed log likelihood
-#'
-#' @param t iteration index
-#' @param obj the EM object with all iterations
-#' @return the log likelihood
-#'
-loglik_obs <- function(t, obj = store_iter) {
-  loglik_obs <- c()
-
-  theta_t <- obj[[t]]$theta
-  mu_t <- obj[[t]]$mu
-  user_K <- length(theta_t)
-
-  # for each N
-  for (u in 1:data$U) {
-    resp_i <- c()
-    for (k in 1:user_K) {
-      resp_i[k] = foreach(j = 1:data$D, .combine = "*") %:%
-        foreach(l = 0:(data$L), .combine = "*") %do% {
-          log(mu_t[k, j, (l + 1)])^(data$uy[u, j] == l)
-        }
-    }
-    loglik_obs[u] <- data$n_u[u] * log(theta_t %*% resp_i)
-  }
-  sum(loglik_obs)
-}
-
-
-
-#'extract parameters from iteration t
-#' @param t iteration index
-#' @param obj the EM object with all iterations
-#'
-#' @return A tibble
-vector_params <- function(t, loglik = FALSE, obj = store_iter) {
-  theta_vector <- obj[[t]]$theta
-  mu_vector <- obj[[t]]$mu[, , (data$L + 1)]
-  df_i <- tibble(param_id = 1:(length(theta_vector) + length(mu_vector)),
-                 type = c(rep("theta", length(theta_vector)),
-                          rep("mu",    length(mu_vector))),
-                 values = c(theta_vector, mu_vector)
-  )
-  if (loglik) df_i$loglik_obs <- loglik_obs(t)
-  return(df_i)
-}
-
-# stack all pre-post comparisons
-params_stacked <- foreach(t = 2:length(store_iter), .combine = "bind_rows") %do% {
-  params_t <- vector_params(t, loglik = TRUE)
-  params_tminus1 <- vector_params(t - 1, loglik = FALSE)
-
-  left_join(params_tminus1, params_t,
-            by = c("param_id", "type"),
-            suffix = c("_pre", "_now")) %>%
-    mutate(iter = t) %>%
-    mutate(diff = abs(values_now - values_pre))
-}
+params_stacked <- summ_params(store_iter)
+params_ch <- summ_params(ch_iter)
 
 
 # plot max of diff
