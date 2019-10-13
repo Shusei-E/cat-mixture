fe314ca47802f865b6f9384d9c1b3112c69521d8
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-13 16:03:08 -0400

    simulate L > 1 outcomes

--- 01_sim-data.R
+++ 01_sim-data.R
@@ -21,20 +21,38 @@ Z_table <- rmultinom(N, 1, theta)
 Z <-  map_dbl(1:N,  ~which(Z_table[, .x] == 1))
 
 # set theta parameters
-mu <- list(
-  `1` = rep(0.10, D),
-  `2` = rep(0.90, D),
-  `3` = rbeta(D, 2, 5)
+mu
+mu_k <- list(
+  `1` = c(0.05, 0.05, 0.90),
+  `2` = c(0.10, 0.10, 0.80),
+  `3` = c(0.15, 0.20, 0.65)
 )
 
-# each mu vector is K by D
-stopifnot(all(map_lgl(mu, ~length(.x) == D)))
+table(Z)
 
+# data$L
+mu <- array(NA, dim = c(K, D, L + 1))
+for (k in 1:K) {
+  for (j in 1:D) {
+    # scales vary by k primarily, but nosie by j
+    mu[k, j, ] <- mu_k[[k]] + 0.01*k*(j - 1)
+    # rescale
+    mu[k, j, ] <- mu[k, j, ] / sum(mu[k, j, ])
+  }
+}
+
+# Missingness
+m <- array(sample(1:3, size = N*D, replace = TRUE), dim = c(N, D))
 
 # Generate data
 y <- array(NA, dim = c(N, D))
 for (i in 1:N) {
-  y[i, ] <- rbinom(D, size = L, prob = mu[[Z[i]]])
+  for (j in 1:D) {
+    mu_i <- mu[Z[i], j, ]
+    l_not_available <- switch(m[i, j], `1` = 2, `2` = 1, `3` = NA)
+    if (!is.na(l_not_available)) mu_i[l_not_available] <- 0
+    y[i, j] <- sample(0:L, size = 1, prob = mu_i)
+  }
 }
 
 # unique profiles
@@ -58,6 +76,7 @@ data <- list(D = D,
              N = N,
              L = L,
              y = y,
+             m = m,
              uy = unique_y_mat,
              n_u = n_u,
              U = U,
