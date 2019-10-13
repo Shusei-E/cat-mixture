20ccc04e0aaa6afcd7b672df98948abd998cd06a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-13 18:33:07 -0400

    both vers

--- 05_diagnose-EM.R
+++ 05_diagnose-EM.R
@@ -13,12 +13,14 @@ store_iter <- read_rds("data/EM/sim-iterations.Rds")
 
 
 # Diagnostics ---------
-params_stacked <- summ_params(store_iter)
+params_stacked <- summ_params(store_iter, data = data, calc_loglik = FALSE)
+params_stacked <- summ_params(store_iter, data = data, calc_loglik = TRUE) %>%
+  mutate(llobs_scale = loglik_obs / (data$N*data$D))
+
 
 
 # plot max of diff
 params_stacked %>%
-  mutate(llobs_scale = loglik_obs / (data$N*data$D)) %>%
   group_by(iter) %>%
   summarize(`Maximum Change in Parameter (probability scale)` = max(diff),
             `Observed Log Likelihood (per data point)` = unique(llobs_scale)) %>%
@@ -38,4 +40,4 @@ params_stacked %>%
        y = "Metric",
        caption = glue("Note: The parmater vector is the estimated theta's and mu's combined.
                       Higher observed log likelihood and lower sup-norms both indicate better fit."))
-ggsave("figures/sim_EM_change-in-params.pdf", w = 4, h = 4)
+ggsave("figures/sim_EM_change-in-params_iia-method_no-missing.pdf", w = 4, h = 4)
