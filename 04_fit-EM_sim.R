d6dd24877fb184ba3409841f23c3b81aee1e5614
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-16 22:05:22 -0400

    run and save. about 15min to run or calc likelihood for the IIA model

--- 04_fit-EM_sim.R
+++ 04_fit-EM_sim.R
@@ -5,18 +5,19 @@ library(glue)
 # Functions ---
 source("03_define_EM-fun.R")
 
-# default
-data <- read_rds("data/sim-data.Rds")
-store_iter <- cat_mixture(data, user_K = 4, n_iter = 100)
-write_rds(store_iter, "data/EM/sim-iterations.Rds")
 
-# using mnlogit algorithm on full data
-store_iter <- cat_mixture(data, user_K = 2, n_iter = 50, fast = FALSE, IIA = TRUE)
-write_rds(store_iter, "data/EM/sim-iterations_IIA-full.Rds")
 
-rm(data)
-
+# using mlogit algorithm on missing data
 data_miss <- read_rds("data/sim-data_missing.Rds")
-store_iter <- cat_mixture(data_miss, user_K = 2, n_iter = 50, fast = FALSE, IIA = TRUE)
+store_iter <- cat_mixture(data_miss, user_K = 2, n_iter = 50, IIA = TRUE)
 write_rds(store_iter, "data/EM/sim-iterations_IIA-miss.Rds")
 
+
+# using mnlogit algorithm on full data
+data <- read_rds("data/sim-data.Rds")
+store_iter <- cat_mixture(data, user_K = 2, n_iter = 50, IIA = TRUE)
+write_rds(store_iter, "data/EM/sim-iterations_IIA-full.Rds")
+
+# default
+store_iter <- cat_mixture(data, user_K = 4, n_iter = 50)
+write_rds(store_iter, "data/EM/sim-iterations.Rds")
--- 05_diagnose-EM.R
+++ 05_diagnose-EM.R
@@ -52,16 +52,23 @@ trend_stacked <- function(params_stacked, dat = data) {
 }
 
 
+# Calculate metrics  ---------
+pst_3_new <- summ_params(store_iter_3, data = data_miss, IIA = TRUE, calc_loglik = TRUE)
+pst_2_new <- summ_params(store_iter_2, data = data, IIA = TRUE, calc_loglik = TRUE)
+pst_1_new <- summ_params(store_iter_1, data = data, calc_loglik = TRUE)
 
-# Diagnostics ---------
-pst <- summ_params(store_iter, data = data, calc_loglik = TRUE)
-pst_1 <- summ_params(store_iter_1, data = data, calc_loglik = TRUE)
-pst_2 <- summ_params(store_iter_2, data = data, calc_loglik = TRUE)
-pst_3 <- summ_params(store_iter_3, data = data_miss, calc_loglik = TRUE)
+write_rds(pst_3_new, "data/EM/sim-stats_IIA-miss.Rds")
+write_rds(pst_2_new, "data/EM/sim-stats_IIA-full.Rds")
+write_rds(pst_1_new, "data/EM/sim-stats.Rds")
 
 
-trend_stacked(pst)
-trend_stacked(pst_1) + trend_stacked(pst_2, dat = data)
+# View ----
+pst_3 <- read_rds("data/EM/sim-stats_IIA-miss.Rds")
+pst_2 <- read_rds("data/EM/sim-stats_IIA-full.Rds")
+pst_1 <- read_rds("data/EM/sim-stats.Rds")
+
+trend_stacked(pst_1)
+trend_stacked(pst_2)
 trend_stacked(pst_3, dat = data_miss)
 
 bind_rows(mutate(pst_1, method = 1),
@@ -83,40 +90,40 @@ bind_rows(mutate(pst_1, method = 1),
 
 
 # plot max of diff
-ggsave("figures/sim_EM_change-in-params_iia-method_missing.pdf", w = 4, h = 4)
-ggsave("figures/sim_EM_change-in-params_iia-method_no-missing.pdf", w = 4, h = 4)
-
-
-
-params <- read_rds("data/sim-data_missing.Rds")
-params$theta
-
-est <- store_iter[[40]]
-round(est$theta, 2)
-
-store_iia <- read_rds("data/EM/sim-iterations_IIA.Rds")
-store_full <- read_rds("data/EM/sim-iterations.Rds")
-
-for (t in length(store_iia)) {
-  ord_iia <- order(store_iia[[t]]$theta, decreasing = TRUE)
-  store_iia[[t]]$theta <- store_iia[[t]]$theta[ord_iia]
-  store_iia[[t]]$zeta <-  store_iia[[t]]$zeta[, ord_iia]
-  store_iia[[t]]$mu <-    store_iia[[t]]$mu[, ord_iia, ]
-
-  ord_full <- order(store_full[[t]]$theta, decreasing = TRUE)
-  store_full[[t]]$theta <- store_full[[t]]$theta[ord_full]
-  store_full[[t]]$zeta <-  store_full[[t]]$zeta[, ord_full]
-  store_full[[t]]$mu <-    store_full[[t]]$mu[ord_full, , ]
-}
-
-params$mu[1:3, 1, 3]
-round(store_full[[40]]$mu[1:3, 1, 3], 2)
-round(store_iia[[40]]$mu[1:3, 1, 3], 2)
-
-
-cbind(params$Z, round(store_full[[40]]$zeta[, ], 2))[1:5, ]
-
-tibble(as.numeric(params$theta), store_full[[40]]$theta)
-round(store_iia[[40]]$zeta[1:5, ], 2)
-
-
+# ggsave("figures/sim_EM_change-in-params_iia-method_missing.pdf", w = 4, h = 4)
+# ggsave("figures/sim_EM_change-in-params_iia-method_no-missing.pdf", w = 4, h = 4)
+#
+#
+#
+# params <- read_rds("data/sim-data_missing.Rds")
+# params$theta
+#
+# est <- store_iter[[40]]
+# round(est$theta, 2)
+#
+# store_iia <- read_rds("data/EM/sim-iterations_IIA.Rds")
+# store_full <- read_rds("data/EM/sim-iterations.Rds")
+#
+# for (t in length(store_iia)) {
+#   ord_iia <- order(store_iia[[t]]$theta, decreasing = TRUE)
+#   store_iia[[t]]$theta <- store_iia[[t]]$theta[ord_iia]
+#   store_iia[[t]]$zeta <-  store_iia[[t]]$zeta[, ord_iia]
+#   store_iia[[t]]$mu <-    store_iia[[t]]$mu[, ord_iia, ]
+#
+#   ord_full <- order(store_full[[t]]$theta, decreasing = TRUE)
+#   store_full[[t]]$theta <- store_full[[t]]$theta[ord_full]
+#   store_full[[t]]$zeta <-  store_full[[t]]$zeta[, ord_full]
+#   store_full[[t]]$mu <-    store_full[[t]]$mu[ord_full, , ]
+# }
+#
+# params$mu[1:3, 1, 3]
+# round(store_full[[40]]$mu[1:3, 1, 3], 2)
+# round(store_iia[[40]]$mu[1:3, 1, 3], 2)
+#
+#
+# cbind(params$Z, round(store_full[[40]]$zeta[, ], 2))[1:5, ]
+#
+# tibble(as.numeric(params$theta), store_full[[40]]$theta)
+# round(store_iia[[40]]$zeta[1:5, ], 2)
+#
+#
