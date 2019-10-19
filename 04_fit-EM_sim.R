788c5a3c31c829a1fb79dda8fe031300f985a3cc
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-19 13:47:27 -0400

    clear out

--- 04_fit-EM_sim.R
+++ 04_fit-EM_sim.R
@@ -8,18 +8,9 @@ source("03_define_EM-fun.R")
 
 # default
 data <- read_rds("data/sim-data.Rds")
-store_iter_rrr <- cat_mixture(data, user_K = 3, n_iter = 3, fast = TRUE, init = "equal")
-store_iter_cpp <- cat_mixture(data, user_K = 3, n_iter = 3, fast = TRUE, init = "equal")
+store_iter <- cat_mixture(data, user_K = 3, n_iter = 3, fast = TRUE, init = "equal")
 write_rds(store_iter, "data/EM/n300_kmeans-init/sim-iterations.Rds")
 
-store_iter_rrr[[2]]$opts[[6]]
-store_iter_cpp[[2]]$opts[[6]]
-
-as.numeric(store_iter_rrr[[2]]$opts[[6]]) /
-as.numeric(store_iter_cpp[[2]]$opts[[6]])
-
-store_iter_rrr[[3]]$mu[, , 2]
-store_iter_cpp[[3]]$mu[, , 2]
 
 # using mlogit algorithm on missing data
 data_miss <- read_rds("data/sim-data_missing.Rds")
