a553a4c5f2b31370f06e31fe677a38a5e2a3ba40
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-19 13:45:30 -0400

    some manual testing betwen R and cpp implementation. the timings here were 2.100361 secs in original and 0.00127697 secs in cpp.

--- 04_fit-EM_sim.R
+++ 04_fit-EM_sim.R
@@ -6,18 +6,28 @@ library(glue)
 source("03_define_EM-fun.R")
 
 
+# default
+data <- read_rds("data/sim-data.Rds")
+store_iter_rrr <- cat_mixture(data, user_K = 3, n_iter = 3, fast = TRUE, init = "equal")
+store_iter_cpp <- cat_mixture(data, user_K = 3, n_iter = 3, fast = TRUE, init = "equal")
+write_rds(store_iter, "data/EM/n300_kmeans-init/sim-iterations.Rds")
+
+store_iter_rrr[[2]]$opts[[6]]
+store_iter_cpp[[2]]$opts[[6]]
+
+as.numeric(store_iter_rrr[[2]]$opts[[6]]) /
+as.numeric(store_iter_cpp[[2]]$opts[[6]])
+
+store_iter_rrr[[3]]$mu[, , 2]
+store_iter_cpp[[3]]$mu[, , 2]
 
 # using mlogit algorithm on missing data
 data_miss <- read_rds("data/sim-data_missing.Rds")
-store_iter <- cat_mixture(data_miss, user_K = 2, n_iter = 50, IIA = TRUE)
-write_rds(store_iter, "data/EM/sim-iterations_IIA-miss.Rds")
+store_iter <- cat_mixture(data_miss, user_K = 3, n_iter = 50, IIA = TRUE, init = "kmeans")
+write_rds(store_iter, "data/EM/n300_kmeans-init/sim-iterations_IIA-miss.Rds")
 
 
 # using mnlogit algorithm on full data
-data <- read_rds("data/sim-data.Rds")
-store_iter <- cat_mixture(data, user_K = 2, n_iter = 50, IIA = TRUE)
-write_rds(store_iter, "data/EM/sim-iterations_IIA-full.Rds")
+# store_iter <- cat_mixture(data, user_K = 3, n_iter = 50, IIA = TRUE)
+# write_rds(store_iter, "data/EM/sim-iterations_IIA-full.Rds")
 
-# default
-store_iter <- cat_mixture(data, user_K = 4, n_iter = 50)
-write_rds(store_iter, "data/EM/sim-iterations.Rds")
